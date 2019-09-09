8260828e8306267fe77d719040e396af342256b1
Corey Russ <cruss.seq11@gmail.com>
2019-09-08 23:17:24 -0700

    Add execute

--- calculator/src/main/java/edu/csc413/calculator/operators/MultiplyOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/MultiplyOperator.java
@@ -1,10 +1,13 @@
 package edu.csc413.calculator.operators;
 
+import edu.csc413.calculator.evaluator.Operand;
+
 public class MultiplyOperator extends Operator {
     public int priority() {
         return 2;
     }
-    static {
-        operators.put("*", new MultiplyOperator());
+    @Override
+    public Operand execute(Operand op1, Operand op2) {
+        return new Operand(op1.getValue()*op2.getValue());
     }
 }
--- calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
@@ -1,10 +1,20 @@
 package edu.csc413.calculator.operators;
 
+import edu.csc413.calculator.evaluator.Operand;
+
 public class PowerOperator extends Operator {
     public int priority() {
-        return 1;
+        return 3;
     }
-    static {
-        operators.put("^", new PowerOperator());
+    @Override
+    public Operand execute(Operand op1, Operand op2) {
+        int tempVal2 = op2.getValue();
+        int tempVal1 = op1.getValue();
+        int tempVal3 = tempVal1;
+        for (int i = 1; i < tempVal2; i++)
+        {
+            tempVal1 = tempVal1*tempVal3;
+        }
+        return new Operand(tempVal1);
     }
 }
