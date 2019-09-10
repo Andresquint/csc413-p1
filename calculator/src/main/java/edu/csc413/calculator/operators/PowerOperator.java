147c930fd47fcf0c020e2264d90267c6bc8fb974
Corey Russ <cruss.seq11@gmail.com>
2019-09-09 22:34:31 -0700

    minor formatting

--- calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
@@ -6,14 +6,14 @@ public class PowerOperator extends Operator {
     public int priority() {
         return 3;
     }
+
     @Override
     public Operand execute(Operand op1, Operand op2) {
         int tempVal2 = op2.getValue();
         int tempVal1 = op1.getValue();
         int tempVal3 = tempVal1;
-        for (int i = 1; i < tempVal2; i++)
-        {
-            tempVal1 = tempVal1*tempVal3;
+        for (int i = 1; i < tempVal2; i++) {
+            tempVal1 = tempVal1 * tempVal3;
         }
         return new Operand(tempVal1);
     }
